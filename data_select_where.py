fe203f5f8ab1b56484d5e644162ccc1ff8b32314
Ernest Bonat, Ph.D <ernest.bonat@gmail.com>
2018-05-11 15:19:09 -0700

    Update the data_select_where.py file

--- data_select_where.py
+++ data_select_where.py
@@ -1,23 +1,35 @@
 # coding: UTF-8 
 
-from data.database import DatabaseConnector
+import sys
+from database import DatabaseConnector
 import config
+from enum import Enum
 
-class DataEnum:
-        author_id, first_name, last_name, middle_name = range(0, 4)
-        
-if __name__ == '__main__':            
+class AuthorContact(Enum):
+    author_id = 0
+    first_name = 1
+    last_name = 2
+    middle_name = 3 
+    
+def get_author_info(first_name_parameters):
     try:            
         database = DatabaseConnector(config.USER, config.PASSWORD, config.HOST, config.DATABASE)    
         mysql_connect = database.mysql_open_connection()    
-        sql = ("SELECT author_id, first_name, last_name, middle_name FROM author WHERE last_name = %s")
-        paremeters = ('White', )
+        sql = ("SELECT author_id, first_name, last_name, middle_name FROM author WHERE first_name = %s")
+        paremeters = ('John', )
         cursor, data = database.mysql_execute_cursor_select(mysql_connect, sql, paremeters)                    
         if cursor.rowcount > 0:                
             for row in data :
-                print (row[DataEnum.author_id], row[DataEnum.first_name], row[DataEnum.last_name], row[DataEnum.middle_name], sep='\t')     
-    except Exception as ex:     
-        print('An error occurred: ' + str(ex) + '. Contact your System Administrator.')
+                print (row[AuthorContact.author_id.value], row[AuthorContact.first_name.value], row[AuthorContact.last_name.value], row[AuthorContact.middle_name.value], sep='\t')     
+    except:
+        exception_message = sys.exc_info()[0]
+        print("An error occurred. {}".format(exception_message))
     finally:                
         database.mysql_close_cursor(cursor)
-        database.mysql_close_connection(mysql_connect)
+        database.mysql_close_connection(mysql_connect)    
+                    
+if __name__ == '__main__':
+    first_name_parameters = ('John', )
+    get_author_info(first_name_parameters)
+    
+  
